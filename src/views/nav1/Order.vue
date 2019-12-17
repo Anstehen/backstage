7a439527000c182296fae6a1d579bc318ecb6ddc
Anstehen <3239126710@qq.com>
2019-12-17 23:27:01 +0800

    模板搭建 一

--- src/views/nav1/Order.vue
+++ src/views/nav1/Order.vue
@@ -1,6 +1,61 @@
 <template>
-  <div class="order">
-    1111111111111111111111111
+  <div class="order box_two">
+      <el-row>
+        <el-col :span="24">
+          <div class="screen box_ten">
+            <el-form :inline="true" :model="formInline" class="demo-form-inline">
+              <el-form-item label="审批人">
+                <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+              </el-form-item>
+              <el-form-item label="活动区域">
+                <el-select v-model="formInline.region" placeholder="活动区域">
+                  <el-option label="区域一" value="shanghai"></el-option>
+                  <el-option label="区域二" value="beijing"></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary" @click="onSubmit">查询</el-button>
+              </el-form-item>
+            </el-form>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="24">
+          <div class="count box_ten">
+              <el-table :data="tableData" border style="width: 100%">
+                <el-table-column fixed prop="index" label="编号" width="66"> </el-table-column>
+                <el-table-column prop="name" label="昵称" width="120"></el-table-column>
+                <el-table-column prop="id" label="userid" width="160"></el-table-column>
+                <el-table-column prop="sex" label="性别" width="80"></el-table-column>
+                <el-table-column prop="address" label="地址" width="350"></el-table-column>
+                <el-table-column prop="phone" label="电话" width="120"></el-table-column>
+                <el-table-column prop="time" label="登陆时间" width="160"></el-table-column>
+                <el-table-column fixed="right" label="操作" width="266">
+                  <template slot-scope="scope">
+                    <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+                    <el-button type="text" size="small">编辑</el-button>
+                  </template>
+                </el-table-column>
+              </el-table>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="24">
+          <div class="paging box_ten">
+              <el-pagination
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+                :current-page="currentPage"
+                :page-sizes="[100, 200, 300, 400]"
+                :page-size="100"
+                layout="total, sizes, prev, pager, next, jumper"
+                :total="400">
+              </el-pagination>
+          </div>
+        </el-col>
+      </el-row>
   </div>
 </template>
 
@@ -10,11 +65,71 @@ export default {
   name: 'order',
   data(){
     return{
-      
+      formInline: {
+        user: '',
+        region: ''
+      },
+      currentPage:1,
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333,
+        sex:"女",
+        id:1234567890,
+        phone:14725836900,
+        time:"2019-12-17 23:23:06"
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1517 弄',
+        zip: 200333,
+        sex:"女",
+        id:1234567890,
+        phone:14725836900,
+        time:"2019-12-17 23:23:06"
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1519 弄',
+        zip: 200333,
+        sex:"女",
+        id:1234567890,
+        phone:14725836900,
+        time:"2019-12-17 23:23:06"
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1516 弄',
+        zip: 200333,
+        sex:"女",
+        id:1234567890,
+        phone:14725836900,
+        time:"2019-12-17 23:23:06"
+      }]
     }
   },
   methods:{
-    
+    onSubmit() {
+      console.log('submit!');
+    },
+    handleClick(row) {
+      console.log(row);
+    },
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+    }
   }
 }
 </script>
@@ -22,5 +137,19 @@ export default {
   .order{
     width: 100%;
     height: 100%;
+    .count{
+      min-height: 100px;
+      border-radius: 4px;
+    }
+    .screen {
+      min-height: 100px;
+      border-radius: 4px;
+      border: 1px dotted #99a9bf;
+    }
+    .paging{
+      min-height: 60px;
+      border-radius: 4px;
+      border: 1px dotted #99a9bf;
+    }
   }
 </style>
